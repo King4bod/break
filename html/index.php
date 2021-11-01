2244b199c329ca4d02dd88b76fff3becff021f19
King4bod <king4bod1231020@email.com>
2021-11-01 14:34:27 +0300

    a

--- html/index.php
+++ html/index.php
@@ -6,7 +6,7 @@
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <link rel="stylesheet" href="../css/Break2.css"> -->
-    
+    <link rel="stylesheet" href="../css/backgraund.css">
   </head>
   <body>
 
@@ -15,42 +15,42 @@
     <div class="backg">
     <div class="row row-cols-1 row-cols-md-3 g-4">
       <div class="col">
-        <div class="card h-100">
+        <div class="card h-100  bg-secondary  text-white  " >
           <img src="..\image\b1.jpg" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">استراحه الماسه</h5>
+            <p class="card-text">تعريف عن الاستراحه</p>
+            <a href="#" class="btn btn-warning">المزيد من التفاصيل</a>
           </div>
         </div>
       </div>
       <div class="col">
-        <div class="card h-100 bg-dark text-white " >
+        <div class="card h-100 bg-secondary    text-white " >
           <img src="..\image\b2.jpg" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a short card.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">استراحه اليوم</h5>
+            <p class="card-text"></p>
+            <a href="#" class="btn btn-warning">المزيد من التفاصيل</a>
           </div>
         </div>
       </div>
       <div class="col">
-        <div class="card h-100">
+        <div class="card h-100 bg-secondary  text-white " >
           <img src="..\image\b2.jpg" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">استراحه الريف</h5>
+            <p class="card-text"></p>
+            <a href="#" class="btn btn btn-warning">المزيد من التفاصيل</a>
           </div>
         </div>
       </div>
       <div class="col">
-        <div class="card h-100">
+        <div class="card h-100  bg-secondary text-white" >
           <img src="..\image\b3.jpg" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">Card title</h5>  
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">استراحه السلام</h5>  
+            <p class="card-text"></p>
+            <a href="#" class="btn btn-warning">المزيد من التفاصيل</a>
           </div>
         </div>
       </div>
