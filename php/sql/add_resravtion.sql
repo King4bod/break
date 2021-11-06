88f001ee56aac865698508ffc3b9928c3fdadb53
ShadowProgram33 <tete24sfa7@hotmail.com>
2021-11-06 22:42:39 +0300

    sql

--- php/sql/add_resravtion.sql
+++ php/sql/add_resravtion.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 31, 2021 at 09:08 PM
+-- Generation Time: Nov 06, 2021 at 08:41 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
@@ -24,25 +24,34 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `add-resravtion`
+-- Table structure for table `add_resravtion`
 --
 
-CREATE TABLE `add-resravtion` (
-  `id` varchar(222) NOT NULL,
-  `break-name` varchar(222) NOT NULL,
-  `location` varchar(222) NOT NULL,
-  `price` varchar(222) NOT NULL,
-  `phone_number` varchar(222) NOT NULL
+CREATE TABLE `add_resravtion` (
+  `id` int(200) NOT NULL,
+  `break_name` varchar(200) NOT NULL,
+  `location` varchar(200) NOT NULL,
+  `price` int(200) NOT NULL,
+  `phone_number` int(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `add_resravtion`
+--
+
+INSERT INTO `add_resravtion` (`id`, `break_name`, `location`, `price`, `phone_number`) VALUES
+(0, 'Albduah', 'Albduah', 555, 32),
+(11, 'Albduah23', '232', 12121, 12121),
+(545, 'Albduah2', 'break4', 22321, 3215);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `add-resravtion`
+-- Indexes for table `add_resravtion`
 --
-ALTER TABLE `add-resravtion`
+ALTER TABLE `add_resravtion`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `phone_number` (`phone_number`);
 COMMIT;
