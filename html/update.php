c5be769d054dee20769d95076852914331cef493
ShadowProgram33 <tete24sfa7@hotmail.com>
2021-11-21 22:51:16 +0300

    sql and php

--- html/update.php
+++ html/update.php
@@ -16,8 +16,20 @@
   <div class="warning">
             <form action="../php/update.php" method="post">
           
-           
-
+         <?php  include "../php/connect.php";
+           $records = mysqli_query($conn,"select break_name from reservations  where users_id=".$_SESSION['id'].""); // fetch data from database
+        while($data = mysqli_fetch_array($records))
+      {   ?>
+ 
+ <label name="mybreak"  class="form-label">your break </label>
+                <select name="mybreak">
+                  <option value="1"><?php echo $data[0];?></option>
+                 
+                </select>
+                <?php
+      
+      }  ?>
+      <br>
                 <label for="break_name"class="form-label"> Break name</label> 
                 <input class="form-control"
                   type="text"
@@ -25,12 +37,14 @@
                   name="break_name"
                 />
                
-                <label for="Location"class="form-label">Location </label> 
-                <input class="form-control"
-                  type="text"
-                  id="Location"
-                  name="Location"
-                />
+                <label name="Location" id ="Location" class="form-label">Location </label>
+                <select name="Location" id="Location">
+                  <option value="alriyad">الرياض</option>
+                  <option value="jaddh">جدة</option>
+                  <option value="aldmam">الدمام</option>
+                  <option value="jazan">جازان</option>
+                </select>
+                <br>
          
                 <label for="Price"class="form-label"> Price per day </label>
                 <input class="form-control"
